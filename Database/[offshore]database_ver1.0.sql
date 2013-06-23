69042b4f18499684e01d9e011e98210ce0aaa88e
Tuan <dinhtuan0108@gmail.com>
2013-06-23 15:29:09 +0700

    Init Datadabase ver 1.0

---
+++ Database/[offshore]database_ver1.0.sql
@@ -0,0 +1,203 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.24-log : Database - offshore
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`offshore` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `offshore`;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `name` varchar(160) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbnail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbnail_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `categories` */
+
+/*Table structure for table `categories_products` */
+
+DROP TABLE IF EXISTS `categories_products`;
+
+CREATE TABLE `categories_products` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `category_id` int(10) DEFAULT NULL,
+  `product_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `categories_products` */
+
+/*Table structure for table `configs` */
+
+DROP TABLE IF EXISTS `configs`;
+
+CREATE TABLE `configs` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(160) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `value` varchar(160) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `configs` */
+
+/*Table structure for table `coupon_categories` */
+
+DROP TABLE IF EXISTS `coupon_categories`;
+
+CREATE TABLE `coupon_categories` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `category_id` int(10) DEFAULT NULL,
+  `coupon_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `coupon_categories` */
+
+/*Table structure for table `coupon_products` */
+
+DROP TABLE IF EXISTS `coupon_products`;
+
+CREATE TABLE `coupon_products` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) DEFAULT NULL,
+  `coupon_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `coupon_products` */
+
+/*Table structure for table `coupons` */
+
+DROP TABLE IF EXISTS `coupons`;
+
+CREATE TABLE `coupons` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `usage_limit` int(10) DEFAULT NULL,
+  `usage_per_customer` int(10) DEFAULT NULL,
+  `expiration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `coupons` */
+
+/*Table structure for table `manufacturers` */
+
+DROP TABLE IF EXISTS `manufacturers`;
+
+CREATE TABLE `manufacturers` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(160) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `thumbnail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbnail_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `manufacturers` */
+
+/*Table structure for table `pages` */
+
+DROP TABLE IF EXISTS `pages`;
+
+CREATE TABLE `pages` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `is_actived` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `pages` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `catetogory_id` int(10) DEFAULT NULL,
+  `sku` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `short_description` text COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `manufacturer_id` int(10) DEFAULT NULL,
+  `price` decimal(12,0) DEFAULT NULL,
+  `small_image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `small_image_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbnail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thumbnail_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `weight` decimal(12,0) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `is_in_stock` tinyint(4) DEFAULT NULL,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `products` */
+
+/*Table structure for table `search_fulltexts` */
+
+DROP TABLE IF EXISTS `search_fulltexts`;
+
+CREATE TABLE `search_fulltexts` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) DEFAULT NULL,
+  `data_index` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `search_fulltexts` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `is_active` tinyint(4) DEFAULT NULL,
+  `role` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
